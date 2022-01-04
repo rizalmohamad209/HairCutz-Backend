844db30aecba3c9358c20c444b0294f5743323c6
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-01-05 02:25:34 +0700

    022

--- app/models/modelHair.py
+++ app/models/modelHair.py
@@ -3,7 +3,7 @@ from app import db
 
 class Hair(db.Model):
     id_hair = db.Column(db.Integer, primary_key=True)
-    image = db.Column(db.Integer)
+    image = db.Column(db.String(200))
     nama_model = db.Column(db.String(200))
 
     def __init__(self, image, nama_model):
